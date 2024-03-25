66726296af35759b9a57b440226de55af0aa6c11
Feliped23 <83152743+Feliped21@users.noreply.github.com>
2024-03-25 18:47:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//Sai maldição
+console.log("Olá mundo");
+
+var x = 10;
+var y = 10;
+console.log(x == y);
+
+const a = 100;
+console.log(x*y+a);
+
+console.log(x=10);
+console.log(x===y);
+
+if (x >= 5) {
+    console.log("o valor é maior que 5.")
+} else {
+    console.log("o valor é menor que 5.")
+}
+
+if (y >= 20) {
+    console.log("o valor é maior que 20.")
+} else {
+    console.log("o valor é menor que 20.")
+}
+resultado = x + y;
+console.log(resultado);
+console.log("bem vindo felipe!");
